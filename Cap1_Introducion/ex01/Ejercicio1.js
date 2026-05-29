a5ecea599abf3d7beb51031e091696812118221f
anderson0101ec <jairerazo419@gmail.com>
2026-05-28 20:13:50 -0500

    subiendo

---
+++ Cap1_Introducion/ex01/Ejercicio1.js
@@ -0,0 +1,8 @@
+/*
+Esto sirve para realizar comentarios teoricos, para escribir texto que no se ejecutara como codigo, se utiliza para escribir comentarios largos o para comentar varias lineas de codigo.
+*/
+
+// Comentarios de una sola linea, se utiliza para escribir comentarios breves o para comentar una sola linea de codigo.
+
+document.write("Hola Mundo"); 
+console.log("Hola Mundo");
