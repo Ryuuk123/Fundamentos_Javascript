2329cbc4a6473283cd8d04e2181d3656ebf697b7
anderson0101ec <jairerazo419@gmail.com>
2026-06-09 19:29:33 -0500

    js

---
+++ javascript/00.js
@@ -0,0 +1,20 @@
+/*
+Instrutor: Daniel Martinez Rayan
+*/
+
+// Laboratorio guiado de codificacion con javascript
+
+// Escribe exactamente el codigo indicado.
+// No cambies nada que no se te pida cambiar.
+//Ejecute el codigo que esta en consola para ver el resultado de cada ejercicio.
+// Inspecciona el navegador para ver los resultados  arrojados por la consola.
+// Observa y analiza el resultado en consola o navegador
+
+/* 
+Este laboratorio se concentra en:
+Sintaxis
+Escritura de codigo
+Observacion de resultados
+Formacion de memoria muscular
+Familiarizacion con herramientas
+*/
