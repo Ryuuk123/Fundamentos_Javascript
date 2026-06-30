47a2f19f86720be0a84d626fb85076de7545ec6b
Ryuu123 <gatilandia513@gmail.com>
2026-06-30 15:41:12 -0500

    cap06

---
+++ javascript/cap06.js
@@ -0,0 +1,18 @@
+// If 
+let age = 20;
+if (age >= 18) {
+    console.log("mayor de edad");
+}
+
+// If else if
+let nota = 4;
+
+if (nota >= 4.5) {
+    console.log("Excelente");
+}
+else if (nota >= 3) {
+    console.log("Aprobado");
+}
+else {
+    console.log("Reprobado")
+}
